0cec6da7abba650deff939d1e4a4129e32cbc7c8
Amitai <amitaiyacobi@gmail.com>
2020-10-16 13:39:09 +0300

    deleted build from git

--- build/CMakeFiles/anomaly-detection.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libanomaly-detection.a"
-)
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/amitai/ass1/Anomaly-Detection
-# Build directory: /home/amitai/ass1/Anomaly-Detection/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
